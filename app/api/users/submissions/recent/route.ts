0f1634e30c78430db0d5e8e7733754c694f86910
Darknight18-Code <ravindrasingh23693@gmail.com>
2026-05-21 22:04:44 +0530

    fix2.4

--- app/api/users/submissions/recent/route.ts
+++ app/api/users/submissions/recent/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/newdb";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const { userId } = await auth();
